4d98927f230046d745b74fd0cf7ff4fe96879f9e
Singh, Bhanu Pratap <BhanuPratap_Singh@intuit.com>
2017-08-13 23:34:32 +0530

    Adding Code

---
+++ hybridFramework/src/main/java/com/hybridFramework/excelTutorial/ReadDataFromExcelSheet.java
@@ -0,0 +1,58 @@
+package com.hybridFramework.excelTutorial;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Iterator;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ReadDataFromExcelSheet {
+
+	
+	  public static void main(String[] args) 
+	    {
+	        try
+	        {
+	            FileInputStream file = new FileInputStream(new File("demo.xlsx"));
+	 
+	            //Create Workbook instance holding reference to .xlsx file
+	            XSSFWorkbook workbook = new XSSFWorkbook(file);
+	 
+	            //Get first/desired sheet from the workbook
+	            XSSFSheet sheet = workbook.getSheetAt(1);
+	 
+	            //Iterate through each rows one by one
+	            Iterator<Row> rowIterator = sheet.iterator();
+	            while (rowIterator.hasNext()) 
+	            {
+	                Row row = rowIterator.next();
+	                //For each row, iterate through all the columns
+	                Iterator<Cell> cellIterator = row.cellIterator();
+	                 
+	                while (cellIterator.hasNext()) 
+	                {
+	                    Cell cell = cellIterator.next();
+	                    //Check the cell type and format accordingly
+	                    switch (cell.getCellType()) 
+	                    {
+	                        case Cell.CELL_TYPE_NUMERIC:
+	                            System.out.print(cell.getNumericCellValue() + " ");
+	                            break;
+	                        case Cell.CELL_TYPE_STRING:
+	                            System.out.print(cell.getStringCellValue() + " ");
+	                            break;
+	                    }
+	                }
+	                System.out.println("");
+	            }
+	            file.close();
+	        } 
+	        catch (Exception e) 
+	        {
+	            e.printStackTrace();
+	        }
+	    }
+}
