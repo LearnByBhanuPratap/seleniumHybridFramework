8850c886f20ab070295bc7ac39269a954e854bf5
Singh, Bhanu Pratap <BhanuPratap_Singh@intuit.com>
2017-08-13 23:22:55 +0530

    Adding Code

---
+++ hybridFramework/src/main/java/com/hybridFramework/excelTutorial/UpdateTestResultInExcel.java
@@ -0,0 +1,50 @@
+package com.hybridFramework.excelTutorial;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class UpdateTestResultInExcel {
+
+	public static void updateResult(String excellocation, String sheetName, String testCaseName, String testStatus)
+			throws IOException {
+
+		FileInputStream file = new FileInputStream(new File(excellocation));
+
+		// Create Workbook instance holding reference to .xlsx file
+		XSSFWorkbook workbook = new XSSFWorkbook(file);
+
+		// Get first/desired sheet from the workbook
+		XSSFSheet sheet = workbook.getSheet(sheetName);
+		// count number of active tows
+		int totalRow = sheet.getLastRowNum() + 1;
+		// count number of active columns in row
+		for (int i = 1; i < totalRow; i++) {
+			XSSFRow r = sheet.getRow(i);
+			String ce = r.getCell(1).getStringCellValue();
+			if (ce.contains(testCaseName)) {
+
+				r.createCell(2).setCellValue(testStatus);
+				file.close();
+
+				System.out.println("done");
+				FileOutputStream outFile = new FileOutputStream(new File(excellocation));
+				workbook.write(outFile);
+				outFile.close();
+				break;
+			}
+
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		String excellocation = System.getProperty("user.dir") + "/demo.xlsx";
+		updateResult(excellocation, "TestReport", "Registration", "Pass");
+		updateResult(excellocation, "TestReport", "Payment", "Fail");
+		updateResult(excellocation, "TestReport", "CancelTest", "Pass");
+	}
+}
